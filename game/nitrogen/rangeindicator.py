82ba62600aaee03f60f1d87677843404bb87c15b
Mitchell Stokes <mogurijin@gmail.com>
2017-04-17 19:34:38 -0700

    Cleanup range indicator logic

--- game/nitrogen/rangeindicator.py
+++ game/nitrogen/rangeindicator.py
@@ -3,11 +3,11 @@ import math
 import panda3d.core as p3d
 
 
-_SDF_SIZE = 128
+_SDF_SIZE = 64
 
 
-def _lin_remap(value, low1, high1, low2, hight2):
-    return low2 + (value - low1) * (hight2 - low2) / (high1 - low1)
+def _lin_remap(value, low1, high1, low2, high2):
+    return low2 + (value - low1) * (high2 - low2) / (high1 - low1)
 
 
 def _make_sdf_circle():
@@ -38,13 +38,11 @@ def _make_sdf_box():
     sdfimg = p3d.PNMImage(_SDF_SIZE, _SDF_SIZE)
 
     for imgx in range(_SDF_SIZE):
-        normx = (imgx / _SDF_SIZE) * 2.0 - 1.0
-        absx = abs(normx)
+        normx = abs((imgx / _SDF_SIZE) * 2.0 - 1.0)
         for imgy in range(_SDF_SIZE):
-            normy = (imgy / _SDF_SIZE) * 2.0 - 1.0
-            absy = abs(normy)
+            normy = abs((imgy / _SDF_SIZE) * 2.0 - 1.0)
 
-            dist = _lin_remap(max(absx, absy), 0, 1, 0.5, 1)
+            dist = _lin_remap(1 - max(normx, normy), 0, 1, 0.5, 1)
             sdfimg.setXel(imgx, imgy, dist)
 
     sdftex = p3d.Texture()
@@ -82,15 +80,16 @@ in vec2 texcoord;
 
 out vec4 o_color;
 
-const float outline_dist = 0.47; // Between 0 and 0.5, 0 = thick outline, 0.5 = no outline
-const float smoothing = 0.001;
-const float debug_alpha = 0.2;
+const float outline_size = 0.05;
+const float smoothing = 0.005;
 
 void main() {
     float dist = texture(sdftex, texcoord).r;
-    float outline_factor = smoothstep(0.5 - smoothing, 0.5 + smoothing, dist);
+    float outline_dist = 0.5 + outline_size;
+
+    float outline_factor = smoothstep(outline_dist - smoothing, outline_dist + smoothing, dist);
     vec4 color = mix(outline_color, ricolor, outline_factor);
-    float alpha = smoothstep(outline_dist - smoothing, outline_dist + smoothing, dist);
+    float alpha = smoothstep(0.5 - smoothing, 0.5 + smoothing, dist);
 
     o_color = vec4(color.rgb, color.a * alpha);
 }
@@ -103,14 +102,17 @@ _SHADER = p3d.Shader.make(p3d.Shader.SL_GLSL, _RI_VERT, _RI_FRAG)
 class RangeIndicator:
     def __init__(self, shape, **kwargs):
         sdftex = None
+        frame = p3d.LVector4(-1, 1, -1, 1)
+        scale = p3d.LVector3(1, 1, 1)
+        offset = p3d.LVector3(0, 0, 0)
 
         if shape == 'circle':
             sdftex = _SDF_CIRCLE
-            frame = p3d.LVector4(-1, 1, -1, 1) * kwargs['radius']
+            scale *= kwargs['radius']
         elif shape == 'box':
             sdftex = _SDF_BOX
-            halfwidth = kwargs['width'] / 2.0
-            frame = p3d.LVector4(-halfwidth, halfwidth, 0, kwargs['length'])
+            scale = p3d.LVector3(kwargs['width'] / 2.0, 1, kwargs['length'] / 2.0)
+            offset = p3d.LVector3(0, kwargs['length'] / 2.0, 0)
         else:
             raise ValueError("Unknown shape for RangeIndicator: {}".format(shape))
 
@@ -119,6 +121,8 @@ class RangeIndicator:
 
         card = p3d.NodePath(cardmaker.generate())
         card.set_p(-90)
+        card.set_scale(scale)
+        card.set_pos(offset)
         card.set_transparency(p3d.TransparencyAttrib.MAlpha)
 
         card.set_texture(sdftex)
@@ -126,7 +130,7 @@ class RangeIndicator:
         card.set_shader(_SHADER)
         card.set_shader_input('sdftex', sdftex)
         card.set_shader_input('ricolor', p3d.LVector4(0.8, 0.0, 0.0, 0.3))
-        card.set_shader_input('outline_color', p3d.LVector4(0.3, 0.0, 0.0, 0.8))
+        card.set_shader_input('outline_color', p3d.LVector4(0.0, 0.0, 0.0, 0.8))
 
         self.graphics = card
 
