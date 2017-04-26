1b2831bef0523d2463b2340c9cda0b2ad458404a
Mitchell Stokes <mogurijin@gmail.com>
2017-04-25 21:53:51 -0700

    Add some files to start playing with deployment
    NOTE: Produces builds, but the build application fails to run due to an
    ImportError.

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+from game.blenderpanda import pman
+
+
+pman.build()
+
+
+setup(
+    name='nitrogen',
+    options = {
+        'build_apps': {
+            'copy_paths': [
+                ('game', '.'),
+                '.pman',
+            ],
+            'gui_apps': {
+                'nitrogen-game': 'game/main.py',
+            },
+            'deploy_platforms': [
+                'manylinux1_x86_64',
+            ],
+            'include_modules': {
+                'nitrogen-game': [
+                    'bamboo.rendermanager'
+                ],
+            },
+            'exclude_modules': {
+                '*': ['game'],
+            },
+        }
+    }
+)
